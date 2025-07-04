7ce308c6f3588f7e070281db0c2bf10127c4178b
François P <francoisplessier@gmail.com>
2025-07-04 19:12:21 +0200

    forgot this one for test

---
+++ tests/pytest/fixtures.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+
+
+@pytest.fixture
+def data_2D():
+    return np.array(
+        [
+            [2.0, 2.0, 2.0],
+            [3.0, 3.0, 3.0],
+            [1.0, 1.0, 1.0],
+            [3.0, 3.0, 3.0],
+            [2.0, 2.0, 2.0],
+            [3.0, 3.0, 3.0],
+        ]
+    )
+
+
+@pytest.fixture
+def subject():
+    return np.array([2, 3, 1, 3, 2, 3])
+
+
+@pytest.fixture
+def data_list():
+    x1 = np.ones((1, 3)) * 1
+    x2 = np.ones((2, 3)) * 2
+    x3 = np.ones((3, 3)) * 3
+    return [x1, x2, x3]

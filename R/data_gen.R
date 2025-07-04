6df7ffa84f7b5c0eeb8d3a98845bcc4311e18caf
François P <francoisplessier@gmail.com>
2025-07-04 18:39:53 +0200

    more difficult observation cases

--- R/data_gen.R
+++ R/data_gen.R
@@ -25,11 +25,15 @@
   b3 <- rnorm(n_sub, mean = 0., sd = 0.8)[ID]
 
   yf <- B1 * x1 + B2 * x2 + B3 * (x3 * x1)
+  # response with NA
   yf[ID == 1 & time == 1] <- NA
-  yf[ID == 2 & time == 2] <- NA
-  yf[ID == 3 & time == 3] <- NA
-  yr <- b1 * x1 + b2 * x2 + b3 * (x3 * x1)
+  yf[ID == 2 & time == 1] <- NA
+  # random effect with less covariates than fixed effects
+  yr <- b1 * x1 + b2 * x2 #
   ym <- yf + yr
+  # covariates with NA
+  x1[ID == 1 & time == 1] <- NA
+  x2[ID == 2 & time == 2] <- NA
 
   data_mixedml <- data.frame(ID, time, x1, x2, x3, yf, ym)
 

e762201af76172f6fd0bdcef6fc052504d330d7f
François P. <francoisplessier@gmail.com>
2025-09-19 11:45:32 +0200

    Updating the data to add noise (convergence…)

--- R/data_gen.R
+++ R/data_gen.R
@@ -1,3 +1,10 @@
+#' Synthetic mixed effects dataset
+#'
+#' This dataset is used to test and study the mixedML package
+#' The fixed effects, random effects and corresponding mixed effects (with
+#' and without noise) are given.
+"data_mixedml"
+
 .generate_data <- function() {
   set.seed(666)
 
@@ -16,26 +23,29 @@
   x2 <- rnorm(n_obs, 100., 3.)
   x3 <- ID %% 2
 
-  B1 <- 5
+  B0 <- 1.
+  B1 <- 5.
   B2 <- 0.5
-  B3 <- 10
+  B3 <- 10.
 
+  b0 <- rnorm(n_sub, mean = 0., sd = 0.05)[ID]
   b1 <- rnorm(n_sub, mean = 0., sd = 0.5)[ID]
   b2 <- rnorm(n_sub, mean = 0., sd = 0.1)[ID]
-  b3 <- rnorm(n_sub, mean = 0., sd = 0.8)[ID]
 
-  yf <- B1 * x1 + B2 * x2 + B3 * (x3 * x1)
+  yf <- B0 + B1 * x1 + B2 * x2 + B3 * (x3 * x1)
   # response with NA
   yf[ID == 1 & time == 1] <- NA
   yf[ID == 2 & time == 1] <- NA
   # random effect with less covariates than fixed effects
-  yr <- b1 * x1 + b2 * x2 #
-  ym <- yf + yr
+  yr <- b0 + b1 * x1 + b2 * x2
+
+  ym_nonoise <- yf + yr
+  ym <- yf + yr + rnorm(n_obs, sd = 0.01 * sd(ym_nonoise, na.rm = TRUE))
   # covariates with NA
   x1[ID == 1 & time == 1] <- NA
   x2[ID == 2 & time == 2] <- NA
 
-  data_mixedml <- data.frame(ID, time, x1, x2, x3, yf, ym)
+  data_mixedml <- data.frame(ID, time, x1, x2, x3, yf, yr, ym, ym_nonoise)
 
   usethis::use_data(data_mixedml, overwrite = TRUE)
   return()

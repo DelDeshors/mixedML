d66d6e67863ebad633d386d40163d57b9c76f6a0
François P. <francoisplessier@gmail.com>
2025-11-20 12:02:10 +0100

    Note to developpers for install

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,9 @@
 .onLoad <- function(libname, pkgname) {
+  # NOTE TO DEV: when calling `devtools::install`, the environment variable defined in MIXED_ML_PYTHON_ENV
+  # is still available… but not the corresponding Python environement
+  # so it's activation will fail.
+  # One just needs to delete this environment variable to make the install work
+  # => `Sys.unsetenv(MIXED_ML_PYTHON_ENV)`
   .activate_environment()
   .modify_pypath()
 }

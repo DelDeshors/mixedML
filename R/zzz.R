5088e529334b5e235913404cf26dedb2b42f7998
François P. <francoisplessier@gmail.com>
2025-11-25 14:42:11 +0100

    Call Python requirement in reticulate

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,12 @@
+PACKAGE_NAME <- "mixedML" # pkgload::pkg_name() does not work with devtools::check
+PYTHON_FOLDER <- "python"
+
 .onLoad <- function(libname, pkgname) {
-  # if reticulate/Python is used, we force the user to create an environment and explicitely declare it to reticulate
-  Sys.setenv(RETICULATE_USE_MANAGED_VENV = "no")
+  # use the requirements.txt file to declare to reticulate the needed libraries with `py_require` to
+  .py_require_from_requirements()
 
   # modifying the PYTHONPATH so the modules defined in inst/python are easily imported
   # and the Python models are recognized when they are loaded
-  PACKAGE_NAME <- "mixedML" # pkgload::pkg_name() does not work with devtools::check
-  PYTHON_FOLDER <- "python"
   py_path <- system.file(PYTHON_FOLDER, package = PACKAGE_NAME)
   if (py_path == "") {
     stop(
@@ -14,4 +15,34 @@
     )
   }
   Sys.setenv(PYTHONPATH = py_path)
+}
+
+
+.py_require_from_requirements <- function() {
+  requirefile <- system.file(paste(PYTHON_FOLDER, "requirements.txt", sep = "/"), package = PACKAGE_NAME)
+  lines <- readLines(requirefile)
+  lines <- lines[lines != "" & !grepl("^#", lines)]
+  # first pass to find python
+  python_ver <- NULL
+  for (line in lines) {
+    grp <- grep("^python[ <=>]", line, perl = TRUE)
+    if (length(grp) > 0) {
+      python_ver <- sub("^python *", "", line)
+      break
+    }
+  }
+  # second pass for the py_require
+  for (line in lines) {
+    grp <- grep("^python[ <=>]", line, perl = TRUE)
+    if (length(grp) > 0) {
+      next
+    }
+    reticulate::py_require(package = line, python_version = python_ver)
+  }
+  return()
+}
+
+.onUnload <- function(libpath) {
+  # clean up the PYTHONPATH modification
+  Sys.unsetenv("PYTHONPATH")
 }
